233306551ec4989f29f99db515ea729a86d7166c
Jhonny Castro Clavijo <johnnycastro72@msn.com>
2022-05-03 01:01:16 -0500

    404
    Adding a 404 page not found

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ import Header from './components/Header'
 import Home from './Pages/Home'
 import About from './Pages/About'
 import Profile from './Pages/Profile'
+import NotFound from './Pages/NotFound'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename='/tutorial' >
       <div className="App">
         <Header />
       </div>
@@ -18,6 +19,7 @@ function App() {
         <Route exact path='/' element={ <Home /> }></Route>
         <Route path='/about' element={ <About /> }></Route>
         <Route path='/profile' element={ <Profile /> }></Route>
+        <Route path='*' element={ <NotFound /> }></Route>
       </Routes>
     </BrowserRouter>
   )
