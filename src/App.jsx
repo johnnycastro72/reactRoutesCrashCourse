f657fc4da45aeb5382aa54b428a29327b050e144
Jhonny Castro Clavijo <johnny.castro@misena.edu.co>
2022-05-03 20:25:16 -0500

    Routes useParam Navigate
    useParam and Navigate methods from routes implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,33 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './App.css'
-import Header from './components/Header'
-import Home from './Pages/Home'
-import About from './Pages/About'
-import Profile from './Pages/Profile'
-import NotFound from './Pages/NotFound'
+import { useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Profile from "./Pages/Profile";
+import Post from "./Pages/Post";
+import NotFound from "./Pages/NotFound";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const [login, setLogin] = useState(false);
 
   return (
-    <BrowserRouter basename='/tutorial' >
+    <BrowserRouter basename="/tutorial">
       <div className="App">
         <Header />
+        <button onClick={() => setLogin(!login)}>{login?'logout':'login'}</button>
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/profile" element={login ? <Profile /> : <Navigate to='/' replace />}></Route>
+          <Route path="/post/:id" element={<Post />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
       </div>
-      <Routes>
-        <Route exact path='/' element={ <Home /> }></Route>
-        <Route path='/about' element={ <About /> }></Route>
-        <Route path='/profile' element={ <Profile /> }></Route>
-        <Route path='*' element={ <NotFound /> }></Route>
-      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
